bc6afe15fc06f805d536fd958ba240d4df2623e7
zisophypsych <ziyaulhaqkhan60@gmail.com>
2023-10-24 09:59:05 +0530

    print only negative nums

---
+++ negative-num.py
@@ -0,0 +1,8 @@
+print("Welcome!, this will print the number if it is negative!")
+
+num = int(input("Enter the number: "))
+
+if num < 0:
+    print(num)
+else:
+    print("It's definetly not a negative Number!")
