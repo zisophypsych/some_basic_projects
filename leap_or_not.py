f41e3275058069038f8e29ed3ea322dd26e10471
zisophypsych <ziyaulhaqkhan60@gmail.com>
2023-10-25 08:32:51 +0530

    Find whether a year is leap or not

---
+++ leap_or_not.py
@@ -0,0 +1,9 @@
+print("Welcome! Let's Find Out Year is Leap or Not!")
+year = int(input("Enter the year: "))
+
+if year%4 == 0:
+    print(f"The Year {year} is a leap year!")
+else:
+    print(f"The Year {year} is not a leap year!")
+
+
