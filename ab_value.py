35b2a9ee3979723a491c8872429adf53716cd139
zisophypsych <ziyaulhaqkhan60@gmail.com>
2023-10-26 10:11:15 +0530

    Print Absolute Value

---
+++ ab_value.py
@@ -0,0 +1,8 @@
+print("Welcome! This will print the absolute value!")
+
+num = int(input("Enter the number: "))
+
+if num < 0:
+    print("Absolute Value: ", num*(-1))
+else:
+    print("Absolute Value: ", num)
