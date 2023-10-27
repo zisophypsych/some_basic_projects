c8ed6835d9c9e6512a510ac53f7eae6b7eb90557
zisophypsych <ziyaulhaqkhan60@gmail.com>
2023-10-27 10:18:11 +0530

    Table of any number

---
+++ table-loop.py
@@ -0,0 +1,8 @@
+print("Welcome! this will print the table of the number you enter!")
+
+num = int(input("ENter the number: "))
+
+i = 1
+while i <= 10:
+    print(f"{num} * {i} = {num*i}")
+    i+=1
