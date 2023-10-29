d3ab9cd8cba1cfe56e237061c4173158c09cb221
zisophypsych <ziyaulhaqkhan60@gmail.com>
2023-10-29 19:25:41 +0530

    Palindrome CHeck

--- palindrome-check.py
+++ palindrome-check.py
@@ -1,6 +1,6 @@
 print("Welcome! This is gonna check whether a number is Palinrome or not!")
 
-num = str(input("Enter the number: "))
+num = str(input("Enter the number!: "))
 n_num = str()
 for i in range(len(num)-1, -1, -1):
     n_num += num[i]
