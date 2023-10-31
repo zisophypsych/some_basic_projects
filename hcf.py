c999c236f4d698d3ef09d4c812eebdd3c070c36c
zisophypsych <ziyaulhaqkhan60@gmail.com>
2023-10-31 08:34:49 +0530

    HCF of two numbers

--- hcf.py
+++ hcf.py
@@ -6,4 +6,22 @@ num_2 = int(input("Enter 2nd number: "))
 if num_1 == num_2:
     print(f"HCF of {num_1} and {num_2} is {num_1}")
 elif num_1 < num_2:
+    while True:
+        if num_2 % num_1 == 0:
+            print(f"HCF of {num_1} and {num_2} is {num_1}")
+            break
+        else:
+            num_2 = num_1
+            num_1 = num2 % num_1
+else:
+    while True:
+        if num_1 % num_2 == 0:
+            print(f"HCF of {num_1} and {num_2} is {num_1}")
+            break
+        else:
+            num_1 = num_2
+            num_2 = num_1 % num_2
+
+
+            
      
